1cac6fcb92540fe838fca1fa7e1ea30ca3e1f69b
bowpowder <165465165+bowpowder@users.noreply.github.com>
2024-04-02 11:43:09 +0330

    created basic username and passwords

--- code.cpp
+++ code.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
 #include<string>
+namespace bp
+{
+    template<typename T>
+    class Array
+    {
+    private:
+        T* heap_alocated_array;
+        int size;
+        int max_size;
+    public:
+        Array(int _size):heap_alocated_array(new T[_size]),size(0),max_size(_size)
+        {
+
+        }
+       
+
+        void push(T value)
+        {
+            if (max_size<=size)
+            {
+                //resize
+                T* temp=new T[max_size*2];
+                max_size*=2;
+                for (int i = 0; i < size; i++)
+                {
+                    temp[i]=heap_alocated_array[i];
+                }
+                delete heap_alocated_array[];
+                heap_alocated_array=temp;                
+            }            
+            heap_alocated_array[size]=value;
+            size++;                       
+        }
+        T* get_array()
+        {
+            return heap_alocated_array;
+        }
+        int get_size()
+        {
+            return size;
+        }
+        ~Array()
+        {
+            delete heap_alocated_array[];
+        }
+    };
+   
+}
 class person
 {
-private:
+protected:
     std::string user_name;
     std::string pass_word;
 public:
@@ -17,8 +65,35 @@ public:
 };
 
 
+
+
+
 int main()
 {
-    std::cout<<"hello wrold"<<std::endl;
+    //teachers
+    int teachers_size=5;
+   bp::Array<person> teachers(teachers_size);
+   for (int i = 0; i < teachers_size; i++)
+   {
+    std::string temp_name="teacher";
+    temp_name+=std::to_string(i);
+    std::string temp_password="pass";
+    temp_password+=std::to_string(i);
+    person temp_person(temp_name,temp_password);
+    teachers.push(temp_person);
+   }
+   //students
+     int students_size=10;
+   bp::Array<person> students(students_size);
+   for (int i = 0; i < students_size; i++)
+   {
+    std::string temp_name="student";
+    temp_name+=std::to_string(i);
+    std::string temp_password="pass";
+    temp_password+=std::to_string(i);
+    person temp_person(temp_name,temp_password);
+    students.push(temp_person);
+   }
+    
     return 0;
 }
