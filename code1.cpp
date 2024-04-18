ab2eb1d3536866cb6a1c5c4dddcf4b8550e0153a
bowpowder <165465165+bowpowder@users.noreply.github.com>
2024-04-18 16:25:03 +0330

    changed code1.cpp

--- code1.cpp
+++ code1.cpp
@@ -1,89 +1,96 @@
 #include <iostream>
 #include <string>
-
-using namespace std;
-
-class questions
+namespace bp
 {
-protected:
-    bool test = false;
-    char type, ans;
-    int a, b, c, d, score, time;
-    string question, answer;
+    enum posible_test_awnsers
+    {
+        a,b,c,d
+    };
+    class test_question
+    {
+    public:
+        posible_test_awnsers currect_anwser;
+        std::string a, b, c, d;
+    public:
+        test_question(std::string option_a={},std::string option_b={},std::string option_c={},std::string option_d={},posible_test_awnsers _currect_anwser =posible_test_awnsers::a):a(option_a),b(option_b),c(option_c),d(option_d),currect_anwser(_currect_anwser)
+        {
+
+        }
+        ~test_question()
+        {
+
+        }
+    }; 
+    class descriptive_question 
+    {
+    public:
+   std::string string_answer;
+    public:
+        descriptive_question (std::string _string_anwser={}): string_answer(_string_anwser)
+        {
+
+        }
+        ~descriptive_question ()
+        {
+
+        }
+    };          
+    
+class question
+{
+public:
+    bool is_test = false;        
+    double score, time;
+   std:: string question_str;
+   void* t_Or_d_question;
 
 public:
-    void kind()
+   question(bool _is_test=false,int _score=0,int _time=0,std::string _question_str={}):is_test(_is_test),score(_score),time(_time),question_str(_question_str),t_Or_d_question(nullptr)
+   {
+     if (is_test)
+     {
+      t_Or_d_question=new test_question();
+     }
+     else
+     {
+      t_Or_d_question=new descriptive_question();
+     }
+
+   } 
+   ~question()
+   {
+    if (is_test)
     {
-        cout << "is it a test?(y/n)\n";
-        cin >> type;
-        switch (type)
-        {
-        case 'y':
-        {
-            test = true;
-            break;
-        }
-        case 'n':
-        {
-            test = false;
-            break;
-        }
-        default:
-        {
-            cout << "please type y or n\n";
-            break;
-        }
-        }
+       test_question* test_ptr=(test_question*)t_Or_d_question;
+       delete test_ptr;
+       test_ptr=nullptr;               
     }
-    void set()
+    else
     {
-        if (test)
-        {
-            cout << "write the qustion\n";
-            getline(cin >> ws, question);
-            cout << "write options\n";
-            cin >> a >> b >> c >> d;
-            cout << "choose correct answer\n";
-            cin >> ans;
-        }
-        else if (!test)
-        {
-            cout << "write the qustion\n";
-            getline(cin >> ws, question);
-            cout << "write the answer\n";
-            getline(cin >> ws, answer);
-        }
-        cout << "Enter the score of question\n";
-        cin >> score;
-        cout << "Enter the question time";
-        cin >> time;
+        descriptive_question* des_ptr=(descriptive_question*)t_Or_d_question;
+        delete des_ptr;
+        des_ptr=nullptr;
     }
+    t_Or_d_question=nullptr;
+   }  
 };
 class exam 
 {
-    int tscore, ttime;
-
+    int total_score, total_time;
+    int number_of_questions;
+    question* questions;
 public:
-    exam()
+    exam(int _number_of_questions=1, int _total_score=0,int _total_time=0):questions(nullptr),total_score(_total_score),total_time(_total_time)
     {
-        cout << "Enter the exam time";
-        cin >> ttime;
-        cout << "Enter the exam score";
-        cin >> tscore;
+
     }
-};
-int main()
-{
-    int n;
-    cout << "Enter the number of questions";
-    cin >> n;
-   questions *p =new num[n];
-    for (int i = 0; i < n; i++)
+    ~exam()
     {
-        num[i].kind();
-        num[i].set();
-    };
-    exam total;
-    delete p;
-    return 0;
+    delete[] questions;
+    }
+ };
+};
+
+int main()
+{ 
 }
