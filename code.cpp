4495f41cfb4f42e8813860dbbf10760fd2f99c28
bowpowder <165465165+bowpowder@users.noreply.github.com>
2024-04-18 16:38:26 +0330

    added questions code to code.cpp
    added and modified questions code

--- code.cpp
+++ code.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include<string>
-#include<fstream>
+#include<fstream>  
 namespace bp
-{
-     
-    
-    
+{     
     template<class T>
     class Array
     {
@@ -115,7 +112,109 @@ namespace bp
     enum save_load_funcs
     {
         students_list_save,students_list_load,students_list_n_s,students_list_n_l,exam_save,exam_load,exam_n_s,exam_n_l
+    };
+
+       enum posible_test_awnsers
+    {
+        a,b,c,d
+    };
+    class test_question
+    {
+    public:
+        posible_test_awnsers currect_anwser;
+        std::string a, b, c, d;
+    public:
+        test_question(std::string option_a={},std::string option_b={},std::string option_c={},std::string option_d={},posible_test_awnsers _currect_anwser =posible_test_awnsers::a):a(option_a),b(option_b),c(option_c),d(option_d),currect_anwser(_currect_anwser)
+        {
+
+        }
+        ~test_question()
+        {
+
+        }
+    }; 
+    class descriptive_question 
+    {
+    public:
+   std::string string_answer;
+    public:
+        descriptive_question (std::string _string_anwser={}): string_answer(_string_anwser)
+        {
+
+        }
+        ~descriptive_question ()
+        {
+
+        }
+    };          
+    
+class question
+{
+public:
+    bool is_test = false;        
+    double score, time;
+   std:: string question_str;
+   void* t_Or_d_question;
+
+public:
+   question(bool _is_test=false,int _score=0,int _time=0,std::string _question_str={}):is_test(_is_test),score(_score),time(_time),question_str(_question_str),t_Or_d_question(nullptr)
+   {
+     if (is_test)
+     {
+      t_Or_d_question=new test_question();
+     }
+     else
+     {
+      t_Or_d_question=new descriptive_question();
+     }
+
+   } 
+   ~question()
+   {
+    if (is_test)
+    {
+       test_question* test_ptr=(test_question*)t_Or_d_question;
+       delete test_ptr;
+       test_ptr=nullptr;               
     }
+    else
+    {
+        descriptive_question* des_ptr=(descriptive_question*)t_Or_d_question;
+        delete des_ptr;
+        des_ptr=nullptr;
+    }
+    t_Or_d_question=nullptr;
+   }  
+};
+class exam 
+{
+    public:
+    int total_score, total_time;    
+    Array<question> questions;
+public:
+    exam(int _number_of_questions=1, int _total_score=0,int _total_time=0):questions(_number_of_questions),total_score(_total_score),total_time(_total_time)
+    {
+
+    }
+    ~exam()
+    {    
+
+    }
+    void calculate_t_time()
+    {
+        int temp_int=0;
+        int size=number_of_questions();
+        for (int i = 0; i < size; i++)
+        {
+           temp_int+=questions[i].time;
+        }
+        total_time=temp_int;        
+    }
+    int number_of_questions()
+    {
+        return questions.get_size();
+    }
+ };
 }
 
 void read_file(std::string path);
